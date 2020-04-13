3d6926db34614e2095744345708b366c955cd661
Luis Ramirez <me.luisramirez@gmail.com>
2020-04-13 13:23:46 +0200

    fix: problem with indentation and clean code for new version

--- extract_emails.py
+++ extract_emails.py
@@ -3,8 +3,11 @@
 import re
 import requests
 
-# Get emails function
+
 def run_emails():
+    '''
+    Get emails function
+    '''
     filepath = 'domains.txt'
     with open(filepath) as fp:
         for line in fp:
@@ -12,23 +15,31 @@ def run_emails():
                 r = requests.get("http://%s" % line)
                 r.encoding = 'ISO-8859-1'
                 page_1 = r.text
-                match = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b', page_1)
+                match = re.findall(
+                    r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b', page_1)
                 with open("emails.txt", "a") as f:
                     for s in match:
                         f.write(str(s) + "\n")
-
+                # Remove duplicates
                 removeDups("emails.txt", "emails.txt")
-    Continue()
+                print('\n'
+                      'Thanks for using this Script! If you enjoyed this script, start the repo http://github.com/luisramirez-m/simple-email-scraper')
+                quit()
+            except:
+                quit()
 
-# Remove Duplicate
+
 def removeDups(inputfile, outputfile):
+    '''
+    Remove Duplicate
+    '''
     lines = open(inputfile, 'r').readlines()
     lines_set = set(lines)
     out = open(outputfile, 'w')
     for line in lines_set:
         out.write(line)
 
-# Main function
+
 def main():
     print("==============")
     print("What option do you want?")
@@ -48,26 +59,5 @@ def main():
     else:
         pass
 
-# Function to continue
-
-
-def Continue():
-    keep_going = input('Do you want to keep going? Enter yes or no. \n'
-                       '').lower()
-    # evaluates user's response.
-    if keep_going == 'yes':
-        main()
-    elif keep_going == 'no':
-        print('\n'
-              'Thanks for using this Script! If you enjoyed this script, start the repo http://github.com/luisramirez-m/simple-email-scraper')
-        quit()
-    else:
-        print('\n'
-              'Input not recognized. Try again.')
-
-
-# Runs the main function, which runs everything else.
-if __name__ == "__main__":
-    main()
 
 main()
